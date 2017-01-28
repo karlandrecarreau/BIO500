e7e9f1b5323427e7dfb6728143452a0ab6350a32
Steve Vissault <s.vissault@yahoo.fr>
2017-01-28 11:35:47 -0500

    clean gh-pages

--- _build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for dir in $(ls -d cours*); do
-    cd $dir
-    rm -rf ./pres/.cache
-    cat *.Rmd > ./pres/index.Rmd
-    Rscript --no-site-file --no-init-file ./scripts/get_slidify.R
-    cd ..
-done
